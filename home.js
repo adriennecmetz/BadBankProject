1188ec7f251caece9477f36369652a6079dfbd30
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-11 14:49:49 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,12 @@
+function Home(){
+  return (
+    <Card
+      bgcolor="secondary"
+      txtcolor="light"
+      header="BadBank Project"
+      title="Welcome to Bad Bank"
+      text="Login To Begin"
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
