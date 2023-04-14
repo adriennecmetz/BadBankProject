5ac8cf946b90968a3285d804abe0854cb97eb446
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 13:23:18 -0700

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,17 +1,49 @@
-function Withdraw() {
-  const ctx = React.useContext(UserContext);
+function Balance(){
+  const ctx = React.useContex(UserContext);
 
-  function handle () {
-      ctx.users.push({name: data.name, withdraw: data.withdraw});
-      return true;
-  }
-
-  return (
-      <Card
+    return(
+        <Card 
         bgcolor="success"
-        header="Withdraw"
-        withdraw={handle}
-        submitButtonWithdraw="Withdrawl Successful"
-      />
-  )
+        header="Balance"
+        // eslint-disable-next-line no-restricted-globals
+        status={status}
+        body={show ?
+            <BalanceForm setShow={setShow}/>:
+                <BalanceMsg setShow={setShow}/>}
+    /> 
+    )
+}
+
+function BalanceMsg(props){
+    return(<>
+    <h5>Your balance has been sent to your email</h5>
+    <button type="submit"
+    className="btn btn-light"
+    onClick={() =>props.setShow(true)}>Back</button>
+    </>);
+}
+
+function BalanceForm(props){
+    const [email, setEmail]        = React.useState('');
+    
+
+    function handle(){
+      ctx.users.push({ name: data.name, balance: data.balance });
+      return true;
+    }
+  
+    return (<>
+
+   Name<br/>
+    <input type="input"
+    className="form-control"
+    placeholder="Enter name"
+    value={name}
+    onChange={e => setName(e.currentTarget.value)}/><br/>
+
+    
+    <button type="submit"
+    className="btn btn-light"
+    onClick={handle}>Show Balance</button>
+    </>);
 }
