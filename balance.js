6e13b9c76c33583bf0a9c1c68412776259b2024e
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 16:12:41 -0700

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,55 +1,32 @@
-function Balance(){
-    const [show, setShow]      = React.useState(true);
-    const [status, setStatus]  = React.useState(' ');
+function Balance() {
+  const [show, setShow] = React.useState(true);
+  const [status, setStatus] = React.useState('');
+  const ctx = React.useContext(UserContext);
+  let balance = ctx.users[ctx.currentUserIndex].balance;
+  let userName = ctx.users[ctx.currentUserIndex].name;
 
-    return(
-        <Card 
+  console.log(`Balance of ${userName} is ${balance}`);
+
+  function handle() {
+    ctx.users.push({ name: data.name, balance: data.balance });
+    return true;
+  }
+
+  return (
+    <>
+      <Card
         bgcolor="success"
         header="Balance"
         status={status}
-        body={show ?
-            <BalanceForm setShow={setShow}/>:
-                <BalanceMsg setShow={setShow}/>}
-    /> 
-    )
-}
-
-function BalanceMsg(props){
-    return(<>
-    <h5>Your balance has been sent to your email</h5>
-    <button type="submit"
-    className="btn btn-light"
-    onClick={() =>props.setShow(true)}>Back</button>
-    </>);
-}
-
-function BalanceForm(props){
-    const [email, setEmail]        = React.useState('');
-    
-
-    function handle(){
-        console.log(email);
-        const url = `/account/balance/${email}`;
-        (async () => {
-            var res  = await fetch(url);
-            var data = await res.json();
-            console.log(data);
-        })();
-        props.setShow(false);
-    
-    }
-    return (<>
-
-    Email address<br/>
-    <input type="input"
-    className="form-control"
-    placeholder="Enter email"
-    value={email}
-    onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    
-    <button type="submit"
-    className="btn btn-light"
-    onClick={handle}>Show Balance</button>
-    </>);
+        body={show ? <BalanceForm setShow={setShow} /> : <BalanceMsg setShow={setShow} />}
+      />
+      Name
+      <br />
+      <input type="input" className="form-control" placeholder="Enter name" value={name} onChange={(e) => setName(e.currentTarget.value)} />
+      <br />
+      <button type="submit" className="btn btn-light" onClick={handle}>
+        Show Balance
+      </button>
+    </>
+  );
 }
