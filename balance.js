5df92e80d327f94107cb7e316569a362d2a4a982
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 14:18:38 -0700

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,41 +1,38 @@
-function Balance() {
+function Withdraw() {
   const ctx = React.useContext(UserContext);
-  const [data, setData] = React.useState({name: '', balance: 0});
+  const [data, setData] = React.useState({withdraw: 0});
 
   function handleChange(event) {
-    setData({...data, balance: event.target.value});
+    setData({...data, withdraw: event.target.value});
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const userIndex = ctx.users.findIndex(user => user.name === data.name);
-    if (userIndex === -1) {
+    const user = ctx.users.find(user => user.name === ctx.currentUser);
+    if (!user) {
       alert('User not found');
-      return false;
+      return;
     }
-    const user = {...ctx.users[userIndex]};
-    user.balance += parseFloat(data.balance);
-    const updatedUsers = [...ctx.users];
-    updatedUsers[userIndex] = user;
-    ctx.setUsers(updatedUsers);
-    setData({name: '', balance: 0});
+    const newBalance = user.balance - parseFloat(data.withdraw);
+    if (newBalance < 0) {
+      alert('Insufficient balance');
+      return;
+    }
+    user.balance = newBalance;
+    setData({withdraw: 0});
     return true;
   }
 
   return (
     <Card
       bgcolor="success"
-      header="Balance"
-      submitButtonLabel="Update"
+      header="Withdraw"
+      submitButtonLabel="Withdraw Balance"
       onSubmit={handleSubmit}
     >
       <div className="form-group">
-        <label htmlFor="name">Name:</label>
-        <input type="text" className="form-control" id="name" onChange={event => setData({...data, name: event.target.value})} value={data.name}/>
-      </div>
-      <div className="form-group">
         <label htmlFor="balance">Balance:</label>
-        <input type="number" className="form-control" id="balance" onChange={handleChange} value={data.balance}/>
+        <input type="number" className="form-control" id="balance" onChange={handleChange} value={data.withdraw}/>
       </div>
     </Card>
   );
