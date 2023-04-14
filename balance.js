0dd16a482fe235ef8ea4c7c1ec26d8e5849eefd7
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 14:27:45 -0700

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,39 +1,65 @@
 function Balance() {
   const ctx = React.useContext(UserContext);
-  const [data, setData] = React.useState({name: '', balance: 0});
+  const [data, setData] = React.useState({ name: '', balance: 0 });
 
   function handleChange(event) {
-    setData({...data, balance: event.target.value});
+    setData({ ...data, balance: event.target.value });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const user = ctx.users.find(user => user.name === data.name);
+    const user = ctx.users.find((user) => user.name === data.name);
     if (!user) {
       alert('User not found');
       return;
     }
     user.balance += parseFloat(data.balance);
     console.log(`Updated balance for ${user.name}: ${user.balance}`);
-    setData({name: '', balance: 0});
-    return true;
+    setData({ name: '', balance: 0 });
   }
 
   return (
-    <Card
-      bgcolor="primary"
-      header="Balance"
-      submitButtonLabel="Update"
-      onSubmit={handleSubmit}
-    >
-      <div className="form-group">
-        <label htmlFor="name">Name:</label>
-        <input type="text" className="form-control" id="name" onChange={event => setData({...data, name: event.target.value})}/>
-      </div>
-      <div className="form-group">
-        <label htmlFor="balance">Balance:</label>
-        <input type="number" className="form-control" id="balance" onChange={handleChange} value={data.balance}/>
-      </div>
-    </Card>
+    <>
+      {ctx.users.map((user, i) => (
+        <Card
+          key={i}
+          bgcolor="success"
+          txtcolor="white"
+          header={`All data Summary: ${user.name}`}
+          text={`User ${i + 1}`}
+          allData={userInfo(user)}
+        />
+      ))}
+      <Card
+        bgcolor="primary"
+        txtcolor="white"
+        header="Balance"
+        submitButtonLabel="Update"
+        onSubmit={handleSubmit}
+      >
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            onChange={(event) =>
+              setData({ ...data, name: event.target.value })
+            }
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="balance">Balance:</label>
+          <input
+            type="number"
+            className="form-control"
+            id="balance"
+            onChange={handleChange}
+            value={data.balance}
+          />
+        </div>
+      </Card>
+    </>
   );
 }
+
