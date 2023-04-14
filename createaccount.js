f987f08d505d0636d88199b3bcdef74fb8743855
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 12:48:26 -0700

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -33,30 +33,35 @@ function CreateAccount() {
 
 
 
+
+
+
   
   return (
-    <Card
+    <><Card
       bgcolor="success"
       header="Create Account"
       status={status}
       body={show ? (
         <>
-        Name </br>
-        <input type = "input" className ="form-control" id="name"
-         placeholder="Enter name" value ={name} 
-         onChange= {e => setName(e.currentTarget.value)} /><br/>
+          Name </>
+        ,
+        <input type="input" className="form-control" id="name"
+          placeholder="Enter name" value={name}
+          onChange={e => setName(e.currentTarget.value)} />, <br />)
 
-        Email </br>
-        <input type = "input" className ="form-control" id="email"
-         placeholder="Enter email" value ={email} 
-         onChange= {e => setEmail(e.currentTarget.value)} /><br/>
-        
-        Password </br>
-        <input type = "input" className ="form-control" id="password"
-         placeholder="Enter password" value ={password} 
-         onChange= {e => setPassword(e.currentTarget.value)} /><br/>
-        
-        <button type ="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+        :
+
+      }
+
+      Email br>
+      <input type="input" className="form-control" id="email"
+        placeholder="Enter email" value={email}
+        onChange={e => setEmail(e.currentTarget.value)} /><br />
+
+      Password </br><input type="input" className="form-control" id="password"
+        placeholder="Enter password" value={password}
+        onChange={e => setPassword(e.currentTarget.value)} /><br /><button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button></>
         </>
           
       ):(
