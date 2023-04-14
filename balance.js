ff712e3b193f26d7a6581de4fe3d4111be5ceece
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 13:21:54 -0700

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,55 +1,17 @@
-function Balance(){
-    const [show, setShow]      = React.useState(true);
-    const [status, setStatus]  = React.useState(' ');
+function Withdraw() {
+  const ctx = React.useContext(UserContext);
 
-    return(
-        <Card 
+  function handle () {
+      ctx.users.push({name: data.name, withdraw: data.withdraw});
+      return true;
+  }
+
+  return (
+      <Card
         bgcolor="success"
-        header="Balance"
-        status={status}
-        body={show ?
-            <BalanceForm setShow={setShow}/>:
-                <BalanceMsg setShow={setShow}/>}
-    /> 
-    )
-}
-
-function BalanceMsg(props){
-    return(<>
-    <h5>Your balance has been sent to your email</h5>
-    <button type="submit"
-    className="btn btn-light"
-    onClick={() =>props.setShow(true)}>Back</button>
-    </>);
-}
-
-function BalanceForm(props){
-    const [email, setEmail]        = React.useState('');
-    
-
-    function handle(){
-        console.log(email);
-        const url = `/account/balance/${email}`;
-        (async () => {
-            var res  = await fetch(url);
-            var data = await res.json();
-            console.log(data);
-        })();
-        props.setShow(false);
-    
-    }
-    return (<>
-
-    Email address<br/>
-    <input type="input"
-    className="form-control"
-    placeholder="Enter email"
-    value={email}
-    onChange={e => setEmail(e.currentTarget.value)}/><br/>
-
-    
-    <button type="submit"
-    className="btn btn-light"
-    onClick={handle}>Show Balance</button>
-    </>);
+        header="Withdraw"
+        withdraw={handle}
+        submitButtonWithdraw="Withdrawl Successful"
+      />
+  )
 }
