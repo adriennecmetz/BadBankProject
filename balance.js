497c67ef349ce70f15f56172035a46247428931c
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 14:21:05 -0700

    Update balance.js

--- balance.js
+++ balance.js
@@ -14,17 +14,26 @@ function Balance() {
       return;
     }
     user.balance += parseFloat(data.balance);
-    console.log(user.balance);
+    console.log(`Updated balance for ${user.name}: ${user.balance}`);
     setData({name: '', balance: 0});
     return true;
   }
 
   return (
     <Card
-      bgcolor="success"
+      bgcolor="primary"
       header="Balance"
       submitButtonLabel="Update"
       onSubmit={handleSubmit}
     >
       <div className="form-group">
         <label htmlFor="name">Name:</label>
+        <input type="text" className="form-control" id="name" onChange={event => setData({...data, name: event.target.value})}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="balance">Balance:</label>
+        <input type="number" className="form-control" id="balance" onChange={handleChange} value={data.balance}/>
+      </div>
+    </Card>
+  );
+}
