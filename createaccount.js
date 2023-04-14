3899c99e34d4d00ea1922b0aa603dc74de78c7d2
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 06:58:12 -0700

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -1,19 +1,66 @@
-function CreateAccount() {
-  const ctx = React.useContext(UserContext);
-  let users = [...ctx.users];
-  let userNumber = users.length;
+function CreateAccount(){
+  const [show, setShow]         = React.useState(true);
+  const [status, setStatus]     = React.useState('');
+  const [name, setName]         = React.useState('');
+  const [email, setEmail]       = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const ctx = React.useContext(UserContext);  
 
-  function handle(data){
-      ctx.users.push({id: userNumber, name: data.name, email: data.email, password: data.password, balance:100 });
+  function validate(field, label){
+      if (!field) {
+        setStatus('Error must include ' + label);
+        setTimeout(() => setStatus(''),7000);
+        return false;
+      }
       return true;
+  }
+  
+function validatePassword(password) {
+    if (password.length < 8) {
+      setStatus("Error: Password must be at least 8 characters long.");
+      setTimeout(() => setStatus(''),5000);
+      return false;
+    }
+    return true;
+  }
+
+  function handleCreate(){
+    console.log(name,email,password);
+    if (!validate(name,     'name'))     return;
+    if (!validate(email,    'email'))    return;
+    if (!validatePassword(password, 'password')) return;
+    ctx.users.push({name,email,password,balance:100});
+    setShow(false);
+  }    
+
+  function clearForm(){
+    setName('');
+    setEmail('');
+    setPassword('');
+    setShow(true);
   }
 
   return (
-      <Card
-        bgcolor="primary"
-        header="Create Account"
-        handle={handle}
-        submitButton="Create another account"
-      />
+    <Card
+      bgcolor="success"
+      header="Create Account"
+      status={status}
+      body={show ? (  
+              <>
+              Name<br/>
+              <input type="input" className="form-control" id="name" placeholder="Enter name" value={name} onChange={e => setName(e.currentTarget.value)} /><br/>
+              Email address<br/>
+              <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
+              Password<br/>
+              <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
+              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              </>
+            ):(
+              <>
+              <h5>Success</h5>
+              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
+              </>
+            )}
+    />
   )
 }
