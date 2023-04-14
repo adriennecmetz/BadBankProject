9d55aa4fb6af895c1da6e295fa052c35de5285de
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 14:20:17 -0700

    Update balance.js

--- balance.js
+++ balance.js
@@ -1,39 +1,30 @@
-function Withdraw() {
+function Balance() {
   const ctx = React.useContext(UserContext);
-  const [data, setData] = React.useState({withdraw: 0});
+  const [data, setData] = React.useState({name: '', balance: 0});
 
   function handleChange(event) {
-    setData({...data, withdraw: event.target.value});
+    setData({...data, balance: event.target.value});
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    const user = ctx.users.find(user => user.name === ctx.currentUser);
+    const user = ctx.users.find(user => user.name === data.name);
     if (!user) {
       alert('User not found');
       return;
     }
-    const newBalance = user.balance - parseFloat(data.withdraw);
-    if (newBalance < 0) {
-      alert('Insufficient balance');
-      return;
-    }
-    user.balance = newBalance;
-    setData({withdraw: 0});
+    user.balance += parseFloat(data.balance);
+    console.log(user.balance);
+    setData({name: '', balance: 0});
     return true;
   }
 
   return (
     <Card
       bgcolor="success"
-      header="Withdraw"
-      submitButtonLabel="Withdraw Balance"
+      header="Balance"
+      submitButtonLabel="Update"
       onSubmit={handleSubmit}
     >
       <div className="form-group">
-        <label htmlFor="balance">Balance:</label>
-        <input type="number" className="form-control" id="balance" onChange={handleChange} value={data.withdraw}/>
-      </div>
-    </Card>
-  );
-}
+        <label htmlFor="name">Name:</label>
