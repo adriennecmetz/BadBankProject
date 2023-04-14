9bfcdd27d7fdd3c49a2c8bb79d176907554358ff
adriennecmetz <114444415+adriennecmetz@users.noreply.github.com>
2023-04-14 07:22:37 -0700

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -4,27 +4,16 @@ function CreateAccount() {
   let userNumber = users.length;
 
   function handle(data){
-    if (!data.name) {
-      alert("Please enter your name.");
-      return false;
-    } else if (!data.email) {
-      alert("Please enter your email.");
-      return false;
-    } else if (data.password.length < 8) {
-      alert("Your password must be at least 8 characters long.");
-      return false;
-    } else {
-      ctx.users.push({id: userNumber, name: data.name, email: data.email, password: data.password, balance: 100 });
+      ctx.users.push({id: userNumber, name: data.name, email: data.email, password: data.password, balance:100 });
       return true;
-    }
   }
 
   return (
-    <Card
-      bgcolor="primary"
-      header="Create Account"
-      handle={handle}
-      submitButton="Create another account"
-    />
+      <Card
+        bgcolor="primary"
+        header="Create Account"
+        handle={handle}
+        submitButton="Create another account"
+      />
   )
 }
